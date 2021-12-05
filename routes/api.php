201aa2790992aebc25ac78740bbbff247bc462d7
camilovillafuerte <cvillafuerte4120@utm.edu.ec>
2021-12-05 00:45:22 -0500

    respaldo

--- routes/api.php
+++ routes/api.php
@@ -102,15 +102,6 @@ Route::delete('/deleteCon_clau/{id}','App\Http\Controllers\Convenios_clausulasCo
 //Ruta para tabla becas_nivel
 //Route::get('/becas','App\Http\Controllers\Becas_nivelController@consulta');
 Route::get('/becas','App\Http\Controllers\Becas_nivelController@index');//mostrar todos los registros
-
-Route::get('/leer', function ()
-{
-    $resultados=DB::select("SELECT * FROM becas_nivels WHERE ID=?",[1] );
-    foreach ($resultados as $becas_nivel){
-        return $becas_nivel->nombre;
-        
-    }
-});
 Route::get('/becas/{id}','App\Http\Controllers\Becas_nivelController@getBecas_nivelxid');//mostrar  los registros por id
 Route::post('/becas','App\Http\Controllers\Becas_nivelController@store');//crear un registro
 Route::post('/addBecas','App\Http\Controllers\Becas_nivelController@insertBecas_nivel');//crear un registro
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/leer', function ()
+{
+    $resultados=DB::select("SELECT * FROM becas_nivels WHERE ID=?",[1] );
+    foreach ($resultados as $becas_nivel){
+        return $becas_nivel->nombre;
+        
+    }
 });
