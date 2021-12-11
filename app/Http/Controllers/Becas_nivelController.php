6622a21dcc5b06d6ae4f18a0f08ab42822231775
camilovillafuerte <cvillafuerte4120@utm.edu.ec>
2021-12-10 23:15:26 -0500

    respaldo

--- app/Http/Controllers/Becas_nivelController.php
+++ app/Http/Controllers/Becas_nivelController.php
@@ -131,7 +131,7 @@ class Becas_nivelController extends Controller
         $becas2 = DB::table('becas_nivels')
         ->select('id', 'nombre', 'tipo', 'estado','fecha_creacion')
         ->where ('estado="A"') 
-        ->orderBy('id', 'DESC')
+        //->orderBy('id', 'DESC')
         ->get()
         ->toJson();
     
