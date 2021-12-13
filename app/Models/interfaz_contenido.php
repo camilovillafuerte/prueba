3f110affe3bc0da6d9e0064f8056a25bb814a019
camilovillafuerte <cvillafuerte4120@utm.edu.ec>
2021-12-13 18:40:39 -0500

    respaldo

--- app/Models/interfaz_contenido.php
+++ app/Models/interfaz_contenido.php
@@ -12,6 +12,6 @@ class interfaz_contenido extends Model
     protected $fillable = ['id','id_interfazs','nombre','descripcion','urlimagen','estado'];
 
     public function interfaz(){
-        return $this->hasMany('App\Models\interfaz');
+        return $this->hasMany('App\Models\interfaz', 'id_interfazs');
     }
 }
