bcde12c931d39a80416be2d55b3ebea55e1dd8a2
camilovillafuerte <cvillafuerte4120@utm.edu.ec>
2021-12-13 18:30:59 -0500

    respaldo

--- app/Http/Controllers/Interfaz_contenidoController.php
+++ app/Http/Controllers/Interfaz_contenidoController.php
@@ -12,13 +12,12 @@ class Interfaz_contenidoController extends Controller
 
     public function getInterfazconprueba(){
         $interfazcon2 = DB::table('interfaz_contenidos')
-        ->join('interfazs','interfazs.id','=','interfaz_contenidos.id')
+        ->join('interfazs','interfazs.id','=','interfaz_contenidos.id_interfazs')
         ->select('interfasz.nombre','interfazs.pagina',
          'interfaz_contenidos.nombre','interfaz_contenidos.descripcion',
          'interfaz_contenidos.urlimagen','interfaz_contenidos.estado')
         -> where('estado','A') 
         -> get();
-        //dd($interfazcon2);
         return response() -> json ($interfazcon2);
        } 
        
