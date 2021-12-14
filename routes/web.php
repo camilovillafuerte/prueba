a28255fa3fbd5eeb59b9813ff0618dc183fa638c
camilovillafuerte <cvillafuerte4120@utm.edu.ec>
2021-12-14 10:52:57 -0500

    respaldo

--- routes/web.php
+++ routes/web.php
@@ -62,7 +62,7 @@ Route::get('/becas', function()
 });
 
 
-Route::get('/interfaz', function ($pagina)
+Route::get('/interfaz/{pagina}', function ($pagina)
 {
     $inter=DB::table('interfazs')
     -> where ('pagina', 'LIKE', "%pagina%" )
