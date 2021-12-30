00e40919ed220c009b6e8f0f2064524ac42d20ef
Sergio Roger <sergio.lkg.31@gmail.com>
2021-12-30 09:18:41 -0500

    Actualizacion de contraseña

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -166,25 +166,75 @@ class UsuarioController extends Controller{
         $user = (object)$request->usuario;
 
         if($user){
-            $update = Usuario::find($user->id);
 
-            $update->nombres = ucfirst(trim($user->nombres));
-            $update->apellidos = ucfirst(trim($user->apellidos));
-            $update->telefono = trim($user->telefono);
-            $update->correo = trim($user->correo);
-            $update->foto = $user->foto;
-            $update->save();
+            $exisEmail = Usuario::where('correo', trim($user->correo))->first();
 
-            $response = [
-                'estado' => true,
-                'mensaje' => 'Datos de usuario actualizado',
-                'usuario' => $update
-            ];
+            //Verificar si el correo pertenece al usuario y pasa
+            if($exisEmail->id === $user->id){
+                $update = Usuario::find($user->id);
+
+                $update->nombres = ucfirst(trim($user->nombres));
+                $update->apellidos = ucfirst(trim($user->apellidos));
+                $update->telefono = trim($user->telefono);
+                $update->correo = trim($user->correo);
+                $update->foto = $user->foto;
+                $update->save();
+
+                $response = [
+                    'estado' => true,
+                    'mensaje' => 'Datos de usuario actualizado',
+                    'usuario' => $update
+                ];
+            }else{
+                $response = [
+                    'estado' => false,
+                    'mensaje' => 'El correo ya se encuentra registrado',
+                    'usuario' => false
+                ];
+            }
         }else{
             $response = [
                 'estado' => false,
-                'mensaje' => 'Np hay datos para procesar',
+                'mensaje' => 'No hay datos para procesar',
                 'usuario' => false
+            ];
+        }
+
+        return response()->json($response);
+    }
+
+    public function updatePassword(Request $request){
+
+        $datos = (object)$request->usuario;
+        $usuario = false;   $response = [];
+
+        $newPassword = $datos->nueva_clave;
+        $oldPassword = $datos->anterior_clave;
+        $id = $datos->id;
+
+        $usuario = Usuario::find($id);
+
+        if($usuario){
+
+           //Verificar que la contraseña anterior es la correcta y actualizar
+           if(Hash::check($oldPassword, $usuario->contrasena)){
+            $usuario->contrasena = Hash::make($newPassword);
+            $usuario->save();
+
+            $response = [
+                'estado' => true,
+                'mensaje' => 'Contraseña actualizada'
+            ];
+           }else{
+            $response = [
+                'estado' => false,
+                'mensaje' => 'La contraseña de verificación o anterior está incorrecta !!'
+            ];
+           }
+        }else{
+            $response = [
+                'estado' => false,
+                'mensaje' => 'El usuario no existe'
             ];
         }
 
