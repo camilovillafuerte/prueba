0b5a81e60c4be8177aa46e59397cddffcdcc8d34
camilovillafuerte <cvillafuerte4120@utm.edu.ec>
2022-01-25 21:19:36 -0500

    respaldo

--- app/Http/Controllers/InterfazController.php
+++ app/Http/Controllers/InterfazController.php
@@ -118,4 +118,64 @@ class InterfazController extends Controller
 
         return response()->json($response);
     }
+    public function updateCarrosel(Request $request)
+    {
+        $carrosel = (object)$request->carrosel;
+        if($carrosel->id==0)
+        {
+            $newInterfaz=new interfaz_contenido();
+            $newInterfaz->id_interfazs=intval($carrosel->id_interfaz);
+            $newInterfaz->usuario_id=intval($carrosel->usuario_id);
+            $newInterfaz->nombre= ucfirst(trim($carrosel->nombre));
+            $newInterfaz->descripcion= ucfirst(trim($carrosel->descripcion));
+            $newInterfaz->urlimagen=$carrosel->urlimagen;
+            $newInterfaz->estado='A';
+            $newInterfaz->save();
+            $response=[
+                'estado'  => true,
+                'mensaje' => 'Imagen Insertada o Modificado'
+            ];
+        }
+        else
+        {
+            $update = interfaz_contenido::find(intval($carrosel->id));
+            $update->nombre=ucfirst(trim($carrosel->nombre));
+            $update->descripcion=ucfirst(trim($carrosel->descripcion));
+            $update->urlimagen=$carrosel->urlimagen;
+            $update->save();
+            $response=[
+                'estado'  => true,
+                'mensaje' => 'Imagen Insertada o Modificado'
+            ];
+        }
+
+
+        return response()->json($response);
+
+    }
+    public function deleteCarrosel(Request $request)
+    {
+        $carrosel = (object)$request->data;
+        $con=0;
+        foreach($carrosel->eliminar as $eli)
+        {
+            $eliObj = (object)$eli;
+            $update = interfaz_contenido::find(intval($eliObj->id));
+            if($update){
+                $con++;
+                $update->estado='D';
+                $update->save();
+            }
+        }
+        $response=[
+            'estado'  => true,
+            'numero'=>$con,
+            'mensaje' => 'Se Elimino los carruseles!!...'
+        ];
+
+        return response()->json($response);
+
+
+    }
+
 }
