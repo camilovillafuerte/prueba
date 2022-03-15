a52ce8c62ddca7508ec4533ecaf269f7df019d54
camilovillafuerte <cvillafuerte4120@utm.edu.ec>
2022-03-14 21:06:46 -0500

    movilidad

--- routes/api.php
+++ routes/api.php
@@ -356,5 +356,6 @@ Route::get('rolesbecas',[BecasMaestriaDoctoradoController::class,'roles_becas_ma
 //Ruta para inicio de Sesión
 Route::post('login-UTM',[UsuarioController::class,'loginUTM']);
 
-
+//Consultar Datos movilidad
+Route::get('consulta-movilidad/{cedula}',[MovilidadController::class,'consultar']);
 
