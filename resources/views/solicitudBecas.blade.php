646d442662d45be9cda3c911931d0a66db3978f1
camilovillafuerte <cvillafuerte4120@utm.edu.ec>
2022-05-08 21:42:34 -0500

    pdf

--- resources/views/solicitudBecas.blade.php
+++ resources/views/solicitudBecas.blade.php
@@ -141,38 +141,106 @@
   width: 50%;
 }
 
-th, td {
-    border: 1px solid blue;
-    width: 100px;
-    word-wrap: break-word;
-}
+
 
 .item{
     width: 30%;
-    float:left;
-    
-}
-
-.item2{
-    width: 30%;
-    float:left;
+    float:right;
     clear: left;
 }
-.item3{
-    width: 30%;
-    float:left;
-    /* clear: left; */
-}
+
+
 .container{
-    /* width: 793px; */
-    height: 50px; 
-    overflow:hidden;
-     /* border: black 2px dashed;   */
+    width: 660px; 
+    overflow: hidden;
+    height: 230px; 
+    /* border: black 2px dashed;   */
      /* background-color:red;  */
+}
+.container2{
+    width: 660px; 
+    overflow: hidden;
+    height: 118px; 
+    /* border: black 2px dashed;   */
+}
+.container3{
+    width: 660px; 
+    overflow: hidden;
+    height: 170px; 
+    /* border: black 2px dashed;   */
+}
+.container4{
+    width: 660px; 
+    overflow: hidden;
+    height: 150px; 
+    /* border: black 2px dashed;    */
+}
+.container5{
+    width: 660px; 
+    overflow: hidden;
+    /* height: 300px;  */
+     /* border: black 2px dashed;  */
+    /* margin-top: 30px;    */
 }
 .page-break {
     page-break-after: always;
  }
+
+span.a {
+  display: inline-block; /* the default for span */
+  width: 205px;
+  height: 40px; 
+  margin-top: 6px; 
+  padding: 5px;
+  border: 1px solid blue;  
+  /* background-color: yellow;  */
+  text-align: center; 
+  font-size: 18px;
+}
+
+span.b {
+  display: inline-block;
+  width: 205px;
+  height: 30px;
+  padding: 5px;
+  margin-top: 8px;
+  /* margin-bottom: 8px; */
+  border: 1px solid blue;    
+  text-align: center; 
+  font-size: 18px;
+}
+span.c {
+  display: inline-block;
+  width: 648px;
+  height: 30px;
+  padding: 5px;
+  border: 1px solid blue;    
+  text-align: justify; 
+  font-size: 18px;
+  margin-bottom: 1px;
+}
+span.d {
+  display: inline-block; /* the default for span */
+  width: 315px;
+  height: 40px; 
+  margin-top: 6px; 
+  padding: 5px;
+  border: 1px solid blue;  
+  /* background-color: yellow;  */
+  text-align: center; 
+  font-size: 18px;
+}
+span.e {
+  display: inline-block;
+  width: 648px;
+  height: 50px;
+  padding: 5px;
+  border: 1px solid blue;    
+  text-align: justify; 
+  font-size: 18px;
+  margin-bottom: 1px;
+}
+
         </style>
     </head>
     <body>
@@ -195,194 +263,119 @@ th, td {
        <main style="margin-top: 1.5rem;">
 
         <div style="width: 100% !important; padding: 10px; text-align: center; background-color: rgb(63, 151, 67)">
-           <span>DATOS PERSONALES</span>
-        </div>
-        
-        <div class="container" style="width: 100%!important;">
-         
-         <div class="item"; style=" padding: 10px;border: 1px solid black;">
-             <span>Cedula:</span>
-             <span>{{$data->becas->cedula}}</span>
-         </div>
-         <div class="item"; style="padding: 10px;border: 1px solid black;">
-            <span>Nombres:</span>
-            <span>{{$data->becas->nombres}}</span>
-        </div>
-        <div class="item"; style="padding: 10px;border: 1px solid black;">
-            <span>Apellido Paterno:</span>
-            <span>{{$data->becas->apellido1}}</span>
-        </div>
+           <span style="font-weight:bold">DATOS PERSONALES</span>
         </div>
 
-        <div class="container" style="width: 100%!important; margin-top: 5px">
-        <div  class="item";  style="padding: 10px;border: 1px solid black;">
-            <span>Apellido Materno:</span>
-            <span>{{$data->becas->apellido2}}</span>
-        </div>
-        <div class="item"; style="padding: 10px;border: 1px solid black;">
-            <span>Tipo de Sangre:</span>
-            <span>{{$data->becas->tipo_sangre}}</span>
-        </div>
-        <div class="item"; style="padding: 10px;border: 1px solid black;">
-            <span>Fecha de Nacimiento:</span>
-            <span>{{$data->becas->fecha_nacimiento}}</span>
-        </div>
-        </div>
-        <div class="container" style="width: 100%!important;  margin-top: 5px">
-        <div class="item"; style="width: 30%;padding: 10px;border: 1px solid black;">
-            <span>Nacionalidad:</span>
-            <span>{{$data->becas->nacionalidad}}</span>
-        </div>
-        <div class="item"; style="width: 30%;padding: 10px;border: 1px solid black;">
-            <span>Genero:</span>
-            <span>{{$data->becas->genero}}</span>
-        </div>
-        <div class="item"; style="width: 30%;padding: 10px;border: 1px solid black;">
-            <span>Estado civil:</span>
-            <span>{{$data->becas->estado_civil}}</span>
-        </div>
-        <div class="item"; style="width: 30%;padding: 10px;border: 1px solid black;">
-            <span>Discapacidad:</span>
-            <span>{{$data->becas->nombre_discapacidad}}</span>
-        </div>
-        </div>
-        <div class="container" style="width: 100%!important; margin-top: 5px">
-        <div class="item"; style="width: 30%;padding: 10px;border: 1px solid black;">
-            <span>Correo institucional:</span>
-            <span>{{$data->becas->correo_personal_institucional}}</span>
-        </div>
-        <div class="item"; style="width: 30%;padding: 10px;border: 1px solid black;">
-            <span>Correo alternativo:</span>
-            <span>{{$data->becas->correo_personal_alternativo}}</span>
-        </div>
-        </div>
-        
+        <div class="container" style="margin-top: 5px">
+     
+             <span class="a">Cedula: {{$data->becas->cedula}}</span>
+       
+             <span class="a">Nombres: {{$data->becas->nombres}}</span>
+   
+            <span class="a">Apellido Paterno: {{$data->becas->apellido1}}</span>
 
-        <div style="width: 100% !important; padding: 10px; text-align: center; background-color: rgb(63, 151, 67)">
-           <span>CONTACTO DE EMERGENCIA</span>
-        </div>
-        <div class="item"; style="width: 30%;padding: 10px;border: 1px solid black;">
-             <span>Nombres:</span>
-             <span>{{$data->becas->contacto_emergencia_nombres}}</span>
-         </div>
-         <div class="item"; style="width: 33%;padding: 10px;border: 1px solid black;">
-            <span>Apellidos:</span>
-            <span>{{$data->becas->contacto_emergencia_apellidos}}</span>
-        </div>
-        <div class="item"; style="width: 30%;padding: 10px;border: 1px solid black;">
-            <span>Teléfono:</span>
-            <span>{{$data->becas->contacto_emergencia_telefono_1}}</span>
-        </div>
-        <div class="row"; style="width: 30%;padding: 10px;border: 1px solid black;">
-            <span>Teléfono:</span>
-            <span>{{$data->becas->contacto_emergencia_telefono_2}}</span>
+            <span class="a">Apellido Materno: {{$data->becas->apellido2}}</span>
+          
+            <span class="a">Tipo de Sangre: {{$data->becas->tipo_sangre}}</span>
+
+            <span class="a">Fecha de Nacimiento: {{$data->becas->fecha_nacimiento}}</span>
+
+            <span class="a">Nacionalidad: {{$data->becas->nacionalidad}}</span>
+
+            <span class="a">Genero: {{$data->becas->genero}} </span>
+
+            <span class="a">Estado civil: {{$data->becas->estado_civil}}</span>
+            
+            <span class="a">Discapacidad: {{$data->becas->nombre_discapacidad}}</span>
+            
+            <span class="a">Correo institucional: {{$data->becas->correo_personal_institucional}}</span>
+
+            <span class="a">Correo alternativo: {{$data->becas->correo_personal_alternativo}}</span>
+
         </div>
 
         <div style="width: 100% !important; padding: 10px; text-align: center; background-color: rgb(63, 151, 67)">
-           <span>RESIDENCIA</span>
+           <span style="font-weight:bold">CONTACTO DE EMERGENCIA</span>
         </div>
 
-        <div class="item"; style="width: 30%;padding: 10px;border: 1px solid black;">
-             <span>País:</span>
-             <span>{{$data->becas->pais}}</span>
-         </div>
-         <div class="item"; style="width: 30%;padding: 10px;border: 1px solid black;">
-             <span>Provincia:</span>
-             <span>{{$data->becas->provincia}}</span>
-         </div>
-         <div class="item"; style="width: 30%;padding: 10px;border: 1px solid black;">
-             <span>Cantón:</span>
-             <span>{{$data->becas->canton}}</span>
-         </div>
-         <div class="row"; style="width: 30%;padding: 10px;border: 1px solid black;">
-             <span>Calle 1:</span>
-             <span>{{$data->becas->residencia_calle_1}}</span>
-         </div>
-         <div class="item"; style="width: 30%;padding: 10px;border: 1px solid black;">
-             <span>Calle 2:</span>
-             <span>{{$data->becas->residencia_calle_2}}</span>
-         </div>
-         <div class="item"; style="width: 30%;padding: 10px;border: 1px solid black;">
-             <span>Calle 3:</span>
-             <span>{{$data->becas->residencia_calle_3}}</span>
-         </div>
-         <div class="row"; style="width: 30%;padding: 10px;border: 1px solid black;">
-             <span>Teléfono domicilio:</span>
-             <span>{{$data->becas->telefono_personal_domicilio}}</span>
-         </div>
-         <div class="row"; style="width: 30%;padding: 10px;border: 1px solid black;">
-             <span>Télefono Personal:</span>
-             <span>{{$data->becas->telefono_personal_celular}}</span>
-         </div>
-        <div class="page-break"></div>
-         <div style="width: 100% !important; padding: 10px; text-align: center; background-color: rgb(63, 151, 67)">
-           <span>DATOS SOLICITUD BECAS</span>
+        <div div class="container2" style="margin-top: 5px">
+            <span class="a">Nombres: {{$data->becas->contacto_emergencia_nombres}}</span>
+    
+            <span class="a">Apellidos: {{$data->becas->contacto_emergencia_apellidos}}</span>
+      
+            <span class="a">Teléfono: {{$data->becas->contacto_emergencia_telefono_1}}</span>
+          
+            <span class="a">Teléfono: {{$data->becas->contacto_emergencia_telefono_2}}</span>
+            
         </div>
-        <div class="item"; style="width: 30%;padding: 10px;border: 1px solid black;">
-             <span>Facultad:</span>
-             <span>{{$data->becas->nombre_facultad}}</span>
-         </div>
-         <div class="item"; style="width: 30%;padding: 10px;border: 1px solid black;">
-             <span>Modalidad:</span>
-             <span>{{$data->becas->modalidad}}</span>
-         </div>
-         <div class="item"; style="width: 30%;padding: 10px;border: 1px solid black;">
-             <span>Destino:</span>
-             <span>{{$data->becas->tipo_destino}}</span>
-         </div>
-         <div class="row"; style="width: 30%;padding: 10px;border: 1px solid black;">
-             <span>Universidad Destino:</span>
-             <span>{{$data->becas->universidad_destino}}</span>
-         </div>
-         <div class="item"; style="width: 30%;padding: 10px;border: 1px solid black;">
-             <span>Campus Destino:</span>
-             <span>{{$data->becas->campus_destino}}</span>
-         </div>
-         <div class="item"; style="width: 30%;padding: 10px;border: 1px solid black;">
-             <span>Numero Semestre:</span>
-             <span>{{$data->becas->numero_semestre}}</span>
-         </div>
-         <div class="item"; style="width: 30%;padding: 10px;border: 1px solid black;">
-             <span>Fecha Incio:</span>
-             <span>{{$data->becas->fecha_inicio}}</span>
-         </div>
-         <div class="row"; style="width: 30%;padding: 10px;border: 1px solid black;">
-             <span>Fecha Fin:</span>
-             <span>{{$data->becas->fecha_fin}}</span>
-         </div>
-         <div class="item"; style="width: 30%;padding: 10px;border: 1px solid black;">
-             <span>Naturaleza:</span>
-             <span>{{$data->becas->naturaleza}}</span>
-         </div>
-         <div class="item"; style="width: 30%;padding: 10px;border: 1px solid black;">
-             <span>Becas o Apoyo:</span>
-             <span>{{$data->becas->beca_apoyo}}</span>
-         </div>
-         <div class="row"; style="width: 30%;padding: 10px;border: 1px solid black;">
-             <span>Monto:</span>
-             <span>{{$data->becas->monto_referencial}}</span>
-         </div>
+    <div style="width: 100% !important; padding: 10px; text-align: center; background-color: rgb(63, 151, 67)">
+           <span style="font-weight:bold">RESIDENCIA</span>
+    </div>
+        <div div class="container3" style="margin-top: 5px">
+            <span class="a">País: {{$data->becas->pais}}</span>
+    
+            <span class="a">Provincia: {{$data->becas->provincia}}</span>
+      
+            <span class="a">Cantón: {{$data->becas->canton}}</span>
+            
+            <span class="a">Calle 1: {{$data->becas->residencia_calle_1}}</span>
+            
+            <span class="a">Calle 2: {{$data->becas->residencia_calle_2}}</span>
+            
+            <span class="a">Calle 3: {{$data->becas->residencia_calle_3}}</span>
 
-         <div style="width: 100% !important; padding: 10px; text-align: center; background-color: rgb(63, 151, 67)">
+            <span class="a">Teléfono domicilio: {{$data->becas->telefono_personal_domicilio}}</span>
+
+            <span class="a">Teléfono personal: {{$data->becas->telefono_personal_celular}}</span>
+        </div>
+    <div style="width: 100% !important; padding: 10px; text-align: center; background-color: rgb(63, 151, 67)">
+           <span style="font-weight:bold">DATOS SOLICITUD BECAS</span>
+    </div>
+    <div class="container4" style="margin-top: 5px">
+    
+            <span class="a">Facultad: {{$data->becas->nombre_facultad}}</span>
+    
+            <span class="a">Modalidad: {{$data->becas->modalidad}}</span>
+        
+            <span class="a">Destino: {{$data->becas->tipo_destino}}</span>
+   
+            <span class="c">Universidad Destino:{{$data->becas->universidad_destino}}</span>
+           
+             <span class="b">Campus Destino: {{$data->becas->campus_destino}}</span>
+        
+             <span class="b">Numero Semestre: {{$data->becas->numero_semestre}}</span>
+       
+             <span class="b">Fecha Incio: {{$data->becas->fecha_inicio}}</span>
+             </div> 
+            <div class="page-break"></div>
+            <div class="container5" style="margin-top: 30px">
+             
+            <span class="a">Fecha Fin: {{$data->becas->fecha_fin}}</span>
+        
+             <span class="a">Naturaleza: {{$data->becas->naturaleza}}</span>
+        
+             <span class="a">Becas o Apoyo: {{$data->becas->beca_apoyo}}</span>
+        
+             <span class="c">Monto: {{$data->becas->monto_referencial}}</span>
+    
+             </div>
+             
+        <div style="width: 100% !important; padding: 10px; text-align: center; background-color: rgb(63, 151, 67)">
            <span>ESPECIFICACIONES ESPECIALES</span>
         </div>
 
-        <div class="item"; style="width: 30%; height: 5%; padding: 10px;border: 1px solid black;">
-             <span>Tipo Alergias:</span>
-             <span>{{$data->becas->alergias}}</span>
-         </div>
-         <div class="item"; style="width: 30%; height: 5%; padding: 10px;border: 1px solid black;">
-             <span>Especificar Alergias:</span>
-             <span>{{$data->becas->especificar_alergia}}</span>
-         </div>
-         <div class="item"; style="width: 32.8%; height: 5%; padding: 10px;border: 1px solid black;">
-             <span>Enfermedades Cronicas y Tratamiento:</span>
-             <span>{{$data->becas->enfermedades_tratamiento}}</span>
-         </div>
-         <div class="row"; style="width: 30%; height: 5%; padding: 10px;border: 1px solid black;">
-             <span>Poliza de Seguro:</span>
-             <span>{{$data->becas->poliza_seguro}}</span>
-         </div>
+      <div class="container5" style="margin-top: 5px">
+             <span class="d">Tipo Alergias: {{$data->becas->alergias}}</span>
+
+             <span class="d">Poliza de Seguro: {{$data->becas->poliza_seguro}}</span>
+
+             <span class="e">Especificar Alergias: {{$data->becas->especificar_alergia}}</span>
+
+             <span class="e">Enfermedades Cronicas y Tratamiento: {{$data->becas->enfermedades_tratamiento}}</span>
+
+    </div>
+
+        
 
     </main>
         <!-- <footer >
